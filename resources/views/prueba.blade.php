cd429192738c4add1c5897f6f9177aa2f642d757
jordiiba <jordiiba2312@gmail.com>
2017-11-25 08:18:29 -0600

    Inicio

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+
+@section('titulo', 'Inicio')
+
+@section('styles')
+    @parent
+@stop
+
+@section('javascripts')
+    @parent
+@stop
+
+@section('banner')
+    @parent
+@stop
+
+@section('contenido')
+    <p>Hola Mundo!</p>
+@stop
+
+@section('footer')
+    @parent
+@stop
